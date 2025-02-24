df63e15a09861e0a5b3712de0f091e945ae0c9b6
JoseB91 <jose.briones.r@hotmail.com>
2025-02-24 12:30:58 -0500

    Initial Commit

---
+++ LigasEcApp/LigasEcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LigasEcAppApp.swift
+//  LigasEcApp
+//
+//  Created by José Briones on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LigasEcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
