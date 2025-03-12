082eeca9cd5be55d43b0b749beb863c6b3e9714b
JoseB91 <jose.briones.r@hotmail.com>
2025-03-12 12:46:20 -0500

    Composition with cache

---
+++ LigasEcApp/Composer.swift
@@ -0,0 +1,116 @@
+//
+//  Composer.swift
+//  LigasEcApp
+//
+//  Created by José Briones on 10/3/25.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+import os
+import SharedAPI
+import LigasEcAPI
+
+class Composer {
+    private let baseURL: URL
+    private let httpClient: URLSessionHTTPClient
+    private let localTeamLoader: LocalTeamLoader
+    
+    //let logger = Logger(subsystem: "com.joseB91.LatinCoaches", category: "main")
+
+    init(baseURL: URL, httpClient: URLSessionHTTPClient, localTeamLoader: LocalTeamLoader) {
+        self.baseURL = baseURL
+        self.httpClient = httpClient
+        self.localTeamLoader = localTeamLoader
+    }
+    
+    static func makeComposer() -> Composer {
+        let baseURL = URL(string: "https://flashlive-sports.p.rapidapi.com/v1/")!
+        let httpClient = makeHTTPClient()
+        let store = makeStore()
+        let localTeamLoader = LocalTeamLoader(store: store, currentDate: Date.init)
+        
+        return Composer(baseURL: baseURL, httpClient: httpClient, localTeamLoader: localTeamLoader)
+    }
+    
+    private static func makeHTTPClient() -> URLSessionHTTPClient {
+        var apiKey = ""
+        
+        do {
+            apiKey = try KeychainManager.retrieveAPIKey()
+        } catch KeychainError.itemNotFound {
+            if let bundleAPIKey = Bundle.main.infoDictionary?["API_KEY"] as? String,
+               !bundleAPIKey.isEmpty {
+                apiKey = bundleAPIKey
+                
+                try? KeychainManager.saveAPIKey(apiKey)
+                // TODO: Log this --> print("Default API key saved to Keychain")
+            } else {
+                // TODO: Log this --> print("No API key found in Keychain or Bundle")
+                // In a real app, you might want to show an error or prompt for the key
+            }
+        } catch {
+            // TODO: Log this --> print("Error retrieving API key: \(error.localizedDescription)")
+        }
+        
+        return URLSessionHTTPClient(
+            session: URLSession(configuration: .ephemeral),
+            apiKey: apiKey)
+    }
+    
+    private static func makeStore() -> TeamStore {
+        do {
+            return try CoreDataLigasEcStore(
+                storeURL: NSPersistentContainer
+                    .defaultDirectoryURL()
+                    .appendingPathComponent("ligas-ec-store.sqlite"),
+                contextQueue: .background)
+        } catch {
+            // Add logger
+            return InMemoryStore()
+        }
+    }
+    
+    func composeTeamViewModel(for league: League) -> TeamViewModel {
+        let teamLoader: () async throws -> [Team] = { [localTeamLoader] in
+            
+            do {
+                let url = TeamEndpoint.get(seasonId: league.id,
+                                           standingType: "overall", // TODO: Manage constants
+                                           locale: "es_MX", // TODO: Get locale
+                                           tournamentStageId: league.stageId).url(baseURL: self.baseURL)
+                let (data, response) = try await self.httpClient.get(from: url)
+                
+                let teams = try TeamMapper.map(data, from: response)
+                
+                Task {
+                    localTeamLoader.saveIgnoringResult(teams)
+                }
+                
+                return teams
+            } catch {
+               return try localTeamLoader.load()             
+            }
+        }
+        return TeamViewModel(teamLoader: teamLoader)
+    }
+    
+    func composePlayerViewModel(for team: Team) -> PlayerViewModel {
+        let playerLoader: () async throws -> [Player] = {
+            let url = PlayerEndpoint.get(sportId: 1,
+                                       locale: "es_MX",
+                                       teamId: team.id).url(baseURL: self.baseURL)
+            let (data, response) = try await self.httpClient.get(from: url)
+            
+            return try PlayerMapper.map(data, from: response)
+        }
+        return PlayerViewModel(playerLoader: playerLoader)
+    }
+}
+
+private extension TeamCache {
+    func saveIgnoringResult(_ teams: [Team]) {
+        try? save(teams)
+    }
+}
--- LigasEcApp/LigasEcApp.swift
+++ LigasEcApp/LigasEcApp.swift
@@ -15,49 +15,11 @@ import os
 
 @main
 struct LigasEcApp: App {
-    
-    let baseURL = URL(string: "https://flashlive-sports.p.rapidapi.com/v1/")!
-    let httpClient: URLSessionHTTPClient
+    private let composer: Composer
     
     init() {
-        var apiKey = ""
-        
-        do {
-            apiKey = try KeychainManager.retrieveAPIKey()
-        } catch KeychainError.itemNotFound {
-            if let bundleAPIKey = Bundle.main.infoDictionary?["API_KEY"] as? String,
-               !bundleAPIKey.isEmpty {
-                apiKey = bundleAPIKey
-                
-                try? KeychainManager.saveAPIKey(apiKey)
-                // TODO: Log this --> print("Default API key saved to Keychain")
-            } else {
-                // TODO: Log this --> print("No API key found in Keychain or Bundle")
-                // In a real app, you might want to show an error or prompt for the key
-            }
-        } catch {
-            // TODO: Log this --> print("Error retrieving API key: \(error.localizedDescription)")
-        }
-        
-        self.httpClient = URLSessionHTTPClient(
-            session: URLSession(configuration: .ephemeral),
-            apiKey: apiKey)
+        self.composer = Composer.makeComposer()
     }
-    
-    private lazy var logger = Logger(subsystem: "com.joseB91.LatinCoaches", category: "main")
-
-    private lazy var store: TeamStore = {
-        do {
-            return try CoreDataLigasEcStore(
-                storeURL: NSPersistentContainer
-                    .defaultDirectoryURL()
-                    .appendingPathComponent("ligas-ec-store.sqlite"))
-        } catch {
-            assertionFailure("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
-            logger.fault("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
-            return InMemoryStore()
-        }
-    }()
     
     @State private var navigationPath = NavigationPath()
         
@@ -66,39 +28,16 @@ struct LigasEcApp: App {
             NavigationStack(path: $navigationPath) {
                 LeagueView(leagueViewModel: LeagueViewModel(), navigationPath: $navigationPath)
                     .navigationDestination(for: League.self) { league in
-                        TeamView(teamViewModel: composeTeamViewModel(for: league), navigationPath: $navigationPath)
+                        TeamView(teamViewModel: composer.composeTeamViewModel(for: league), navigationPath: $navigationPath)
                     }
                     .navigationDestination(for: Team.self) { team in
-                        PlayerView(playerViewModel: composePlayerViewModel(for: team))
+                        PlayerView(playerViewModel: composer.composePlayerViewModel(for: team))
                     }
             }
         }
     }
-        
-    func composeTeamViewModel(for league: League) -> TeamViewModel {
-        let teamLoader: () async throws -> [Team] = {
-            let url = TeamEndpoint.get(seasonId: league.id,
-                                       standingType: "overall", // TODO: Manage constants
-                                       locale: "es_MX", // TODO: Get locale
-                                       tournamentStageId: league.stageId).url(baseURL: baseURL)
-            let (data, response) = try await httpClient.get(from: url)
-            
-            return try TeamMapper.map(data, from: response)
-        }
-        return TeamViewModel(teamLoader: teamLoader)
-    }
     
-    func composePlayerViewModel(for team: Team) -> PlayerViewModel {
-        let playerLoader: () async throws -> [Player] = {
-            let url = PlayerEndpoint.get(sportId: 1,
-                                         locale: "es_MX",
-                                         teamId: team.id).url(baseURL: baseURL)
-            let (data, response) = try await httpClient.get(from: url)
-            
-            return try PlayerMapper.map(data, from: response)
-        }
-        return PlayerViewModel(playerLoader: playerLoader)
-    }
 }
 
 //TODO: Add Acceptance and UIIntegration tests
+
